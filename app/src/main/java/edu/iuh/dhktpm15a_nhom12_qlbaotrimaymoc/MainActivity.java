fd76890d44c835ee79d6686da9bf7f6c2b32871c
dinhdiepvuong <diepvuong.py@gmail.com>
2022-11-13 16:00:38 +0700

    create IU app

---
+++ app/src/main/java/edu/iuh/dhktpm15a_nhom12_qlbaotrimaymoc/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.iuh.dhktpm15a_nhom12_qlbaotrimaymoc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/edu/iuh/dhktpm15a_nhom12_qlbaotrimaymoc/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package edu.iuh.dhktpm15a_nhom12_qlbaotrimaymoc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
